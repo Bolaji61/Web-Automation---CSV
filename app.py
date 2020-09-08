c74c69caccff7cf4f3d3da0068b0f8937f5ded04
Bolaji61 <balogunbolaji61@gmail.com>
2020-09-08 09:44:16 +0100

    Adjust field_names and optimize code

--- app.py
+++ app.py
@@ -19,53 +19,57 @@ def index():
     return """
         <html>
             <body>
-                <h1>Transform a csv upload</h1>
+            <div style="padding-top: 250px; ">
+                <h1 style="text-align: center; font-size: 2em;"> Extract data from CSV</h1>
 
-                <form action="/upload" method="post" enctype="multipart/form-data">
-                    <div align="center">
-                        <input type="file" name="data_file" />
-                        <input type="submit" name="csv_upload_btn" value="Upload" />
+                <form action="/upload" method="post" enctype="multipart/form-data" >
+                    <div align="center" >
+                        <input type="file" name="data_file" style="font-size:1em;"/>
+                        <br>
+                        <input type="submit" name="csv_upload_btn" value="Upload" style="font-size:0.8em;"/>
                     </div>
                 </form>
+            </div>
             </body>
         </html>
     """
 
 @app.route('/upload', methods=["POST"])
 def upload():
+    cur = mysql.connection.cursor()
+
     csvfile = request.files['data_file']
     if not csvfile:
         return "No file"
 
     stream = io.StringIO(csvfile.stream.read().decode("UTF8"), newline=None)
     data = csv.reader(stream)
-    
+
     next(data)   #skip first line of csv file
-    print(data)
-    # data_dict = []
     for row in data:
-        # data_dict.append({
-        #     username: row[0],
-        #     "login_email": row[1],
-        #     "identifier": row[2],
-        #     "first_name": row[3],
-        #     "last_name": row[4]
-        # })
-        username = row[0]
-        login_email =row[1]
-        identifier = row[2]
-        first_name =row[3]
-        last_name = row[4]
-        print("ROW", row)
-        # print(data_dict)
+        orderDate = row[0]
+        region =row[1]
+        rep = row[2]
+        item =row[3]
+        units = row[4]
+        unitCost = row[5]
+        total = row[6]
         
-    cur = mysql.connection.cursor()
-    cur.execute("INSERT INTO csvdata(username, login_email, identifier, first_name, last_name) VALUES (%s, %s, %s, %s, %s)", \
-        (username, login_email, identifier, first_name, last_name))
+        cur.execute("INSERT INTO csv_data (orderDate, region, rep, item, units, unitCost, total) VALUES (%s, %s, %s, %s, %s, %s, %s)", \
+            (orderDate, region, rep, item, units, unitCost, total))
+    
+        cur.execute('SELECT * FROM csv_data')
+        table_data = cur.fetchall()
+
+
+    num_fields = len(cur.description)
+    field_names = [i[0] for i in cur.description]
     mysql.connection.commit()
     cur.close()
-        
-    return "File uploaded successfully"
+
+    flash("File uploaded successfully")
+    print(table_data)
+    return render_template('index.html', output_data = table_data, field_names=field_names)
 
 if __name__ == '__main__':
     app.run(debug=True)
